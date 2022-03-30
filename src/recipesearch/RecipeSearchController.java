9cbfbc8b892ac49609877a692b7b3d5d62cd55f8
Reshad-Qurishi <reshadqurishi@yahoo.com>
2022-03-30 23:45:26 +0200

    first push

--- src/recipesearch/RecipeSearchController.java
+++ src/recipesearch/RecipeSearchController.java
@@ -4,9 +4,14 @@ package recipesearch;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.FlowPane;
 import se.chalmers.ait.dat215.lab2.Recipe;
 import se.chalmers.ait.dat215.lab2.RecipeDatabase;
@@ -20,7 +25,8 @@ public class RecipeSearchController implements Initializable {
     private ComboBox comboBoxIngrediens;
     @FXML
     private ComboBox comboBoxCuisine;
-
+    @FXML
+    private RadioButton radioButton;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -33,7 +39,22 @@ public class RecipeSearchController implements Initializable {
         comboBoxCuisine.getSelectionModel().select("Visa alla");
 
 
+        /*ToggleGroup difficultyToggleGroup = new ToggleGroup();
+        radioButton.setToggleGroup(difficultyToggleGroup);
+        radioButton.setSelected(true);
+        difficultyToggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
 
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+
+                if (difficultyToggleGroup.getSelectedToggle() != null) {
+                    RadioButton selected = (RadioButton) difficultyToggleGroup.getSelectedToggle();
+                    RecipeBackendController backendController = null;
+                    backendController.setDifficulty(selected.getText());
+                    updateRecipeList();
+                }
+            }
+        }); */
     }
 
     private void updateRecipeList(RecipeBackendController recipeBC) {
